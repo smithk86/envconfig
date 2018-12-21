85202762c5d7931fcfd13c4f993162171e2e429e
Kyle Smith <ksmith@smc3.com>
2018-12-20 19:39:39 -0500

    fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Kyle Smith',
     author_email='smithk86@gmail.com',
     url='https://github.com/smithk86/envconfig',
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    py_modules=['envconfig'],
     extras_require={
         'yaml':  ['PyYAML==3.13'],
         'dateparser': ['dateparser==0.7.0'],
